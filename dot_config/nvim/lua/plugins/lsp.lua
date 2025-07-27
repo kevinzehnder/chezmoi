f9a9b69885c01254327dc2c54af14590402bb818
kevinzehnder <kevin.zehnder@jlmy.ch>
2025-07-27 16:16:02 +0200

    add json schemas

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -124,28 +124,45 @@ return {
 		--  - settings (table): Override the default settings passed when initializing the server.
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 
+
 		local servers = {
 			yamlls = {
 				settings = {
 					yaml = {
 						schemas = {
 							["https://taskfile.dev/schema.json"] = "Taskfile.{yml,yaml}",
-							["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-							["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-							["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-							["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-							["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-							["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-							["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+							["https://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+							["https://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+							["https://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+							["https://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+							["https://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+							["https://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
 							["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
 							["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
 							["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
 							["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
 							["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
-						}
-					}
 
-				}
+							-- Kubernetes schemas
+							["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.29.0-standalone-strict/all.json"] = "*.k8s.{yml,yaml}",
+
+							-- Helm
+							["https://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+							["https://json.schemastore.org/chart-lock"] = "Chart.lock",
+
+							-- ArgoCD
+							["https://raw.githubusercontent.com/argoproj/argo-cd/master/manifests/crds/application-crd.yaml"] = "*application*.{yml,yaml}",
+							["https://raw.githubusercontent.com/argoproj/argo-cd/master/manifests/crds/appproject-crd.yaml"] = "*appproject*.{yml,yaml}",
+						},
+						validate = true,
+						completion = true,
+						hover = true,
+						schemaStore = {
+							enable = true,
+							url = "https://www.schemastore.org/api/json/catalog.json",
+						},
+					}
+				},
 
 			},
 			-- clangd = {},
@@ -238,6 +255,7 @@ return {
 		require("mason-lspconfig").setup({
 			ensure_installed = {
 				"lua_ls",
+				"yamllint",
 			},
 			automatic_installation = {},
 			handlers = {
