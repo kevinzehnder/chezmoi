5da7703b94342f277992e9d09a55bc1113d3897d
kevinzehnder <kevin.zehnder@jlmy.ch>
2026-03-16 10:01:17 +0100

    change editor to zed

--- dot_config/zsh/config.d/core/environment.zsh
+++ dot_config/zsh/config.d/core/environment.zsh
@@ -3,8 +3,8 @@
 #
 
 # Editors
-export EDITOR='nvim'
-export VISUAL='nvim'
+export EDITOR='zeditor --wait'
+export VISUAL='zeditor --wait'
 export COLORTERM="truecolor"
 export NVIM_APPNAME="nvim"
 
