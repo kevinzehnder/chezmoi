7bd47e27ddd8543400b52dcebe0f895577eddbd0
kevinzehnder <kevin.zehnder@jlmy.ch>
2026-04-20 08:48:02 +0200

    remove z alias

--- dot_config/zsh/config.d/core/aliases.zsh
+++ dot_config/zsh/config.d/core/aliases.zsh
@@ -32,8 +32,7 @@ alias svim='sudo -E nvim' # Use -E to preserve environment variables
 alias vim='nvim'
 alias cim='nvim'
 alias v='nvim' # Shorter alias for nvim
-alias z='zeditor'
-# 
+
 # Neovide (WSL-aware)
 if [[ -n "$WSL_DISTRO_NAME" ]]; then
     alias neovide='nohup neovide.exe --wsl --neovim-bin $(which nvim) >/dev/null 2>&1 &'
