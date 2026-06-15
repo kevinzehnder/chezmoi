20bf6bb5c6dbf8511e90218719d09fa591893fa5
kevinzehnder <kevin.zehnder@jlmy.ch>
2026-06-15 08:51:34 +0200

    fix(nvim): restore modern treesitter config

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -4,24 +4,37 @@ return {
 		lazy = false,
 		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"bash",
-					"c",
-					"diff",
-					"go",
-					"html",
-					"lua",
-					"luadoc",
-					"markdown",
-					"markdown_inline",
-					"query",
-					"vim",
-					"vimdoc",
-				},
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
+			local languages = {
+				"bash",
+				"c",
+				"go",
+				"diff",
+				"html",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"vim",
+				"vimdoc",
+			}
+
+			local ts = require("nvim-treesitter")
+			ts.setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
+
+			ts.install(languages)
+
+			local group = vim.api.nvim_create_augroup("UserTreesitterSetup", { clear = true })
+			vim.api.nvim_create_autocmd("FileType", {
+				group = group,
+				callback = function(args)
+					local ok = pcall(vim.treesitter.start, args.buf)
+					if ok then
+						vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
 			})
 		end,
 	},
